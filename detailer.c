a36f285d84d687e5c1d985da3e2380688faf5244
Maxime Henrion <mhenrion@gmail.com>
2003-10-07 14:54:20 +0000

    Oops, actually add the dummy detailer thread.

---
+++ detailer.c
@@ -0,0 +1,59 @@
+/*-
+ * Copyright (c) 2003, Maxime Henrion <mux@FreeBSD.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "config.h"
+#include "detailer.h"
+#include "mux.h"
+
+#define	LINE_MAX	4096
+
+void *
+detailer(void *arg)
+{
+	char buf[LINE_MAX];
+	struct config *config;
+	char *line;
+	size_t in, off;
+	int rdchan, wrchan;
+
+	config = arg;
+	rdchan = config->chan0;
+	wrchan = config->chan0;
+	in = off = 0;
+	for (;;) {
+		line = chan_getln(rdchan, buf, sizeof(buf), &in, &off);
+		assert(line != NULL);
+		printf("%s\n", line);
+	}
+	return (NULL);
+}
