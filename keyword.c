6941bb0eb13551b2766ccb3ff15e0e200af8ae02
Maxime Henrion <mhenrion@gmail.com>
2006-01-30 22:06:02 +0000

    Tidy up some code that calls strptime() to check that we ate the whole string, as we do in rcsdatetotime().

--- keyword.c
+++ keyword.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: projects/csup/keyword.c,v 1.22 2006/01/27 17:13:49 mux Exp $
+ * $FreeBSD: projects/csup/keyword.c,v 1.23 2006/01/27 21:29:07 mux Exp $
  */
 
 #include <assert.h>
@@ -395,11 +395,13 @@ tag_expand(struct tag *tag, struct diff *diff)
 	 */
 	char cvsdate[32];
 	struct tm tm;
-	char *filename, *val;
+	char *cp, *filename, *val;
 
-	if (strptime(diff->d_revdate, "%Y.%m.%d.%H.%M.%S", &tm) == NULL)
-		if (strptime(diff->d_revdate, "%y.%m.%d.%H.%M.%S", &tm) == NULL)
-			err(1, "strptime");
+	cp = strptime(diff->d_revdate, "%Y.%m.%d.%H.%M.%S", &tm);
+	if (cp == NULL)
+		cp = strptime(diff->d_revdate, "%y.%m.%d.%H.%M.%S", &tm);
+	if (cp == NULL || *cp != '\0')
+		err(1, "strptime");
 	if (strftime(cvsdate, sizeof(cvsdate), "%Y/%m/%d %H:%M:%S", &tm) == 0)
 		err(1, "strftime");
 	filename = strrchr(diff->d_rcsfile, '/');
