d3e74b60ced575e41aaa66c740cba35ef348844d
Maxime Henrion <mhenrion@gmail.com>
2006-02-11 19:15:03 +0000

    Return from updater_delete() when fattr_delete() failed, no need to try to remove the ancestor directories.

--- updater.c
+++ updater.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: projects/csup/updater.c,v 1.69 2006/02/10 18:18:47 mux Exp $
+ * $FreeBSD: projects/csup/updater.c,v 1.70 2006/02/11 19:09:28 mux Exp $
  */
 
 #include <sys/types.h>
@@ -285,9 +285,11 @@ updater_delete(struct coll *coll, char *name)
 	if (coll->co_options & CO_DELETE) {
 		lprintf(1, " Delete %s\n", name + coll->co_prefixlen + 1);
 		error = fattr_delete(name);
-		if (error)
+		if (error) {
 			lprintf(-1, "Cannot delete \"%s\": %s\n",
 			    name, strerror(errno));
+			return;
+		}
 		if (coll->co_options & CO_CHECKOUTMODE)
 			updater_prunedirs(coll->co_prefix, name);
 	} else {
